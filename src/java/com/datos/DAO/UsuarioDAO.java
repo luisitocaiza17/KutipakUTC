f964dbb30b12a3880903e58cbb84524c18b623bd
luis smartwork <luisito_caiza17@hotmail.com>
2015-07-20 15:52:08 -0500

    pantalla login ultimo cambio

---
+++ src/java/com/datos/DAO/UsuarioDAO.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.datos.DAO;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import persistencia.tables.Usuario;
+import static persistencia.tables.Usuario.USUARIO;
+import persistencia.tables.records.UsuarioRecord;
+
+/**
+ *
+ * @author luisito
+ */
+public class UsuarioDAO {
+    public List<UsuarioRecord> ConsultarUsuarios() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException{
+        ConectarBD con = new ConectarBD();
+        List<UsuarioRecord> listadoUsuarios= new ArrayList<UsuarioRecord>();
+        Connection conexion= con.realiza_conexion();
+        DSLContext create = DSL.using(conexion, SQLDialect.MYSQL);
+        Result<Record> result = create.select().from(USUARIO).fetch();
+        for(Record r : result){
+            UsuarioRecord losUsuarios = new UsuarioRecord();
+            losUsuarios.setUsuarioid(r.getValue(USUARIO.USUARIOID));
+            losUsuarios.setNombreusuario(r.getValue(USUARIO.NOMBREUSUARIO));
+            losUsuarios.setContrasenia(r.getValue(USUARIO.CONTRASENIA));
+            losUsuarios.setRol(r.getValue(USUARIO.ROL));
+            losUsuarios.setPersonaid(r.getValue(USUARIO.PERSONAID));
+            listadoUsuarios.add(losUsuarios);
+        }
+        conexion.close();
+        return listadoUsuarios;
+    }
+    
+     public List<UsuarioRecord> ConsultarUsuariosEspecificos(UsuarioRecord usuariosp) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException{
+        ConectarBD con = new ConectarBD();
+        List<UsuarioRecord> listadoUsuarios= new ArrayList<UsuarioRecord>();
+        Connection conexion= con.realiza_conexion();
+        DSLContext create = DSL.using(conexion, SQLDialect.MYSQL);
+        Result<Record> result = create.select().from(USUARIO)
+                .where(USUARIO.USUARIOID.equal(usuariosp.getUsuarioid())
+                        .or(USUARIO.ROL.equal(usuariosp.getRol())
+                                .or(USUARIO.PERSONAID.equal(usuariosp.getPersonaid())
+                                        .or(USUARIO.NOMBREUSUARIO.equal(usuariosp.getNombreusuario())
+                                                .or(USUARIO.CONTRASENIA.equal(usuariosp.getContrasenia()))))))
+                .fetch();
+        for(Record r : result){
+            UsuarioRecord losUsuarios = new UsuarioRecord();
+            losUsuarios.setUsuarioid(r.getValue(USUARIO.USUARIOID));
+            losUsuarios.setNombreusuario(r.getValue(USUARIO.NOMBREUSUARIO));
+            losUsuarios.setContrasenia(r.getValue(USUARIO.CONTRASENIA));
+            losUsuarios.setRol(r.getValue(USUARIO.ROL));
+            losUsuarios.setPersonaid(r.getValue(USUARIO.PERSONAID));
+            listadoUsuarios.add(losUsuarios);
+        }
+        conexion.close();
+        return listadoUsuarios;
+    }
+     
+    /*Metodo Para almacenar los Personas*/
+    public boolean grabarUsuarios(UsuarioRecord usuariosp) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException{
+        ConectarBD con = new ConectarBD();
+        Connection conexion = con.realiza_conexion();
+	DSLContext create = DSL.using(conexion, SQLDialect.MYSQL);
+        create.insertInto(Usuario.USUARIO, Usuario.USUARIO.USUARIOID,Usuario.USUARIO.ROL,Usuario.USUARIO.PERSONAID,Usuario.USUARIO.NOMBREUSUARIO,Usuario.USUARIO.CONTRASENIA)
+                .values(usuariosp.getUsuarioid(),usuariosp.getRol(),usuariosp.getPersonaid(),usuariosp.getNombreusuario(),usuariosp.getContrasenia()).execute();
+        conexion.close();
+        return true;
+    }
+    
+     public boolean ActualizarUsuarios(UsuarioRecord usuariosp) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException{
+        ConectarBD con = new ConectarBD();
+        Connection conexion = con.realiza_conexion();
+	DSLContext create = DSL.using(conexion, SQLDialect.MYSQL);
+        create.update(USUARIO).set(USUARIO.ROL,usuariosp.getRol())
+                .set(USUARIO.PERSONAID,usuariosp.getPersonaid())
+                .set(USUARIO.NOMBREUSUARIO,usuariosp.getNombreusuario())
+                .set(USUARIO.CONTRASENIA,usuariosp.getContrasenia())
+                .where(USUARIO.USUARIOID.equal(usuariosp.getUsuarioid())).execute();
+        conexion.close();
+        return true;
+    }
+    
+      public boolean EliminarUsuarios(UsuarioRecord usuariosp) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException{
+        ConectarBD con = new ConectarBD();
+        Connection conexion = con.realiza_conexion();
+	DSLContext create = DSL.using(conexion, SQLDialect.MYSQL);
+        create.delete(USUARIO).where(USUARIO.USUARIOID.equal(usuariosp.getUsuarioid()));
+        return true;
+    }
+}
--- web/jsp/login.jsp
+++ web/jsp/login.jsp
@@ -11,49 +11,49 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Login Page</title>
         <link href="../css/bootstrap/bootstrap.min.css" rel="stylesheet">
-	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
         <link href="../css/bootstrap/styles.css" rel="stylesheet">
     </head>
     <body>
         <div class="row">
-       
-        <div class="col-lg-12 text-center v-center">
-          
-          <h1>HOLA administrador!</h1>
-          <p class="lead">Ingresa tus Credenciales para poder continuar :)</p>
-          
-          <br><br><br>
-          
-          <form class="col-lg-12">
-            <div class="input-group" style="width:340px;text-align:center;margin:0 auto;">
-                <input class="form-control input-lg" title="Don't worry. We hate spam, and will not share your email with anyone." placeholder="Usuario" type="text">
-            </div>
-            <div class="input-group" style="width:340px;text-align:center;margin:0 auto;">
-                <input class="form-control input-lg" title="Don't worry. We hate spam, and will not share your email with anyone." placeholder="Contraseña" type="text">
-            </div>
-               <div class="input-group" style="width:340px;text-align:center;margin:0 auto;">
-                   <input class="form-control btn btn-primary " type="BUTTON" value="INGRESA! :)!">
-            </div>
-          </form>
-        </div>
-        
-      </div> <!-- /row -->
-  
-  	  <div class="row">
-       
-        <div class="col-lg-12 text-center v-center" style="font-size:39pt;">
-          <a href="#"><i class="icon-google-plus"></i></a> <a href="#"><i class="icon-facebook"></i></a>  <a href="#"><i class="icon-twitter"></i></a> <a href="#"><i class="icon-github"></i></a> <a href="#"><i class="icon-pinterest"></i></a>
-        </div>
-      
-      </div>
-  
-  	<br><br><br><br><br>
 
-</div> <!-- /container full -->
+            <div class="col-lg-12 text-center v-center">
 
-	<!-- script references -->
-		<script src="../js/jquery.min.js"></script>
-		<script src="../js/bootstrap.min.js"></script>
-	</body>
-    </body>
+                <h1>HOLA administrador!</h1>
+                <p class="lead">Ingresa tus Credenciales para poder continuar :)</p>
+
+                <br><br><br>
+
+                <form class="col-lg-12">
+                    <div class="input-group" style="width:340px;text-align:center;margin:0 auto;">
+                        <input class="form-control input-lg" title="Don't worry. We hate spam, and will not share your email with anyone." placeholder="Usuario" type="text">
+                    </div>
+                    <div class="input-group" style="width:340px;text-align:center;margin:0 auto;">
+                        <input class="form-control input-lg" title="Don't worry. We hate spam, and will not share your email with anyone." placeholder="Contraseña" type="text">
+                    </div>
+                    <div class="input-group" style="width:340px;text-align:center;margin:0 auto;">
+                        <input class="form-control btn btn-primary " type="BUTTON" value="INGRESA! :)!">
+                    </div>
+                </form>
+            </div>
+
+        </div> <!-- /row -->
+
+        <div class="row">
+
+            <div class="col-lg-12 text-center v-center" style="font-size:39pt;">
+                <a><i class="icon-github"></i></a> <a href="#"><i class="icon-pinterest"></i></a>
+            </div>
+
+        </div>
+
+        <br><br><br><br><br>
+
+    </div> <!-- /container full -->
+
+    <!-- script references -->
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+</body>
 </html>
